3c6351fd084e42a50eda35f61ebaf65fe303e195
Palyasolo <soloduhin.pavel@ya.ru>
2024-07-13 16:28:17 +0700

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,7 @@
+my_dict = {'mama': 1, 'papa': 1, 'brat':0, 'deda':2}
+print (my_dict)
+print( my_dict['mama'])
+print( my_dict.get('sestra'))
+my_dict.update ({'sestra':0, 'baba':2})
+del my_dict['mama']
+print( my_dict)
